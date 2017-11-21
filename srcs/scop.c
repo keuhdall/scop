3d7cba385ebfd96070a563e0c9b734889febaa44
Léonard Marques <lmarques@student.42.fr>
2017-11-21 01:23:47 +0100

    added code for a simple window

--- srcs/scop.c
+++ srcs/scop.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/12 19:05:10 by lmarques          #+#    #+#             */
-/*   Updated: 2017/11/20 01:30:26 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/11/21 01:21:40 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,24 @@
 
 int	main(int argc, char *argv[])
 {
+	GLFWwindow	*win;
+
 	if (!argc)
 		ft_putendl(argv[0]);
-	glfwInit();
+	if (!glfwInit())
+	{
+		ft_putendl("error");
+		return (-1);
+	}
+	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	win = glfwCreateWindow(640, 480, "test", NULL, NULL);
+	if (!win) {
+		ft_putendl("err win");
+		return(-1);
+	}
+	glfwMakeContextCurrent(win);
 	return (0);
 }
