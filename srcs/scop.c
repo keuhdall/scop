905f82c56dafac3bc33ea8226779d284a38a88bd
keuhdall <lmarques@student.42.fr>
2017-11-23 22:29:10 +0100

    fixed NULL value for window

--- srcs/scop.c
+++ srcs/scop.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/12 19:05:10 by lmarques          #+#    #+#             */
-/*   Updated: 2017/11/21 01:21:40 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/11/23 22:24:47 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,18 @@ int	main(int argc, char *argv[])
 		ft_putendl(argv[0]);
 	if (!glfwInit())
 	{
-		ft_putendl("error");
+		ft_putendl("error initializing glfw");
 		return (-1);
 	}
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	win = glfwCreateWindow(640, 480, "test", NULL, NULL);
 	if (!win) {
 		ft_putendl("err win");
-		return(-1);
+		return (-1);
 	}
 	glfwMakeContextCurrent(win);
 	return (0);
