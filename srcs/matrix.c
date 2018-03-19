3793fe1c8247968e498137a202c71436c04a54af
Léonard Marques <lmarques@student.42.fr>
2018-03-19 19:14:38 +0100

    lookat function should be fixed

--- srcs/matrix.c
+++ srcs/matrix.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/17 16:02:22 by lmarques          #+#    #+#             */
-/*   Updated: 2018/03/18 18:07:39 by lmarques         ###   ########.fr       */
+/*   Updated: 2018/03/19 19:13:01 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,26 +66,37 @@ t_vec4	scale(t_vec4 v, t_vec3 pos)
 	return (v);
 }
 
-t_vec4	*lookat(t_vec3 pos, t_vec3 target, t_vec3 upvec)
+t_vec4	*lookat(t_vec3 eye, t_vec3 target, t_vec3 up)
 {
 	t_vec3	v[3];
-	t_vec4	*mat;
+	t_vec4	*orientation;
+	t_vec4	*translation;
+	t_vec4	*res;
 
-	v[0] = normalize(diff3(target, pos));
-	v[1] = normalize(upvec);
-	v[2] = normalize(cross3(v[0], v[1]));
-	mat = new_matrix();
-	mat[0].x = v[2].x;
-	mat[1].x = v[2].y;
-	mat[2].x = v[2].z;
-	mat[0].y = v[1].x;
-	mat[1].y = v[1].y;
-	mat[2].y = v[1].z;
-	mat[0].z = -v[0].x;
-	mat[1].z = -v[0].y;
-	mat[2].z = -v[0].z;
-	mat[3].x = -(dot(v[2], pos));
-	mat[3].y = -(dot(v[1], pos));
-	mat[3].z = dot(v[0], pos);
-	return (mat);
+	v[0] = normalize(diff3(eye, target));    // The "forward" vector.
+	v[1] = normalize(cross3(up, v[0]));// The "right" vector.
+	v[2] = cross3(v[0], v[1]);     // The "up" vector.
+	orientation = new_matrix();
+	translation = new_matrix();
+	res = new_matrix();
+	orientation[0].x = v[1].x;
+	orientation[0].y = v[2].x;
+	orientation[0].z = v[0].x;
+	orientation[1].x = v[1].y;
+	orientation[1].y = v[2].y;
+	orientation[1].z = v[0].y;
+	orientation[2].x = v[1].z;
+	orientation[2].y = v[2].z;
+	orientation[2].z = v[0].z;
+	orientation[3].w = 1;
+	translation[0].x = 1;
+	translation[1].y = 1;
+	translation[2].z = 1;
+	translation[3].x = -eye.x;
+	translation[3].y = -eye.y;
+	translation[3].z = -eye.z;
+	res = mat_prod(orientation, translation);
+	free(orientation);
+	free(translation);
+	return (res);
 }
