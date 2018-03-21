47913f32caf57a824c895236cc8694678eb15576
Léonard Marques <lmarques@student.42.fr>
2018-03-21 17:07:29 +0100

    added perspective funciton

--- srcs/matrix.c
+++ srcs/matrix.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/17 16:02:22 by lmarques          #+#    #+#             */
-/*   Updated: 2018/03/19 19:13:01 by lmarques         ###   ########.fr       */
+/*   Updated: 2018/03/21 15:54:30 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_vec4	*new_matrix(void)
 	t_vec4	*mat;
 
 	if (!(mat = (t_vec4 *)malloc(sizeof(t_vec4) * 4)))
-		return (NULL);
+		puterr(ERR_MALLOC_FAILED);
 	ft_bzero(mat, sizeof(t_vec4) * 4);
 	return (mat);
 }
@@ -46,7 +46,7 @@ t_vec4	*translate(t_vec4 v, t_vec3 pos)
 {
 	t_vec4	*m;
 
-	m = ft_memalloc(sizeof(t_vec4) * 4);
+	m = new_matrix();
 	m[0].x = 1;
 	m[0].w = pos.x;
 	m[1].y = 1;
@@ -66,37 +66,14 @@ t_vec4	scale(t_vec4 v, t_vec3 pos)
 	return (v);
 }
 
-t_vec4	*lookat(t_vec3 eye, t_vec3 target, t_vec3 up)
+void	perspective(t_vec4 *m, float f[4])
 {
-	t_vec3	v[3];
-	t_vec4	*orientation;
-	t_vec4	*translation;
-	t_vec4	*res;
+	float	angle_half;
 
-	v[0] = normalize(diff3(eye, target));    // The "forward" vector.
-	v[1] = normalize(cross3(up, v[0]));// The "right" vector.
-	v[2] = cross3(v[0], v[1]);     // The "up" vector.
-	orientation = new_matrix();
-	translation = new_matrix();
-	res = new_matrix();
-	orientation[0].x = v[1].x;
-	orientation[0].y = v[2].x;
-	orientation[0].z = v[0].x;
-	orientation[1].x = v[1].y;
-	orientation[1].y = v[2].y;
-	orientation[1].z = v[0].y;
-	orientation[2].x = v[1].z;
-	orientation[2].y = v[2].z;
-	orientation[2].z = v[0].z;
-	orientation[3].w = 1;
-	translation[0].x = 1;
-	translation[1].y = 1;
-	translation[2].z = 1;
-	translation[3].x = -eye.x;
-	translation[3].y = -eye.y;
-	translation[3].z = -eye.z;
-	res = mat_prod(orientation, translation);
-	free(orientation);
-	free(translation);
-	return (res);
+	angle_half = tan(f[0] / 2.0f);
+	m[0].x = (1.0f / (f[1] * angle_half));
+	m[1].y = (1.0f / angle_half);
+	m[2].z = (-(f[3] + f[2]) / (f[3] - f[2]));
+	m[3].z = -1.0f;
+	m[2].w = (-(2.0f * f[3] * f[2]) / (f[3] - f[2]));
 }
