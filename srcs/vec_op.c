38dd252e924263a925ac1adadd889d7886d97e89
Léonard Marques <lmarques@student.42.fr>
2018-03-24 00:05:58 +0100

    refactored vector calculations

--- srcs/vec_op.c
+++ srcs/vec_op.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/18 17:28:15 by lmarques          #+#    #+#             */
-/*   Updated: 2018/03/18 17:34:32 by lmarques         ###   ########.fr       */
+/*   Updated: 2018/03/24 00:02:35 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,28 @@
 
 t_vec3	sum3(t_vec3 a, t_vec3 b)
 {
-	a.x += b.x;
-	a.y += b.y;
-	a.z += b.z;
-	return (a);
+	return ((t_vec3){a.x + b.x, a.y + b.y, a.z + b.z});
 }
 
 t_vec3	diff3(t_vec3 a, t_vec3 b)
 {
-	a.x -= b.x;
-	a.y -= b.y;
-	a.z -= b.z;
-	return (a);
+	return ((t_vec3){a.x - b.x, a.y - b.y, a.z - b.z});
 }
 
 t_vec3	prod3(t_vec3 a, t_vec3 b)
 {
-	a.x *= b.x;
-	a.y *= b.y;
-	a.z *= b.z;
-	return (a);
+	return ((t_vec3){a.x * b.x, a.y * b.y, a.z * b.z});
 }
 
 t_vec3	quo3(t_vec3 a, t_vec3 b)
 {
-	a.x /= b.x;
-	a.y /= b.y;
-	a.z /= b.z;
-	return (a);
+	return ((t_vec3){a.x / b.x, a.y / b.y, a.z / b.z});
 }
 
 t_vec3	cross3(t_vec3 a, t_vec3 b)
 {
-	t_vec3	v;
-
-	v.x = a.y * b.z - a.z * b.y;
-	v.y = a.z * b.x - a.x * b.z;
-	v.z = a.x * b.y - a.y * b.x;
-	return (v);
+	return ((t_vec3){
+			a.y * b.z - a.z * b.y,
+			a.z * b.x - a.x * b.z,
+			a.x * b.y - a.y * b.x});
 }
--- srcs/vec_op_const.c
+++ srcs/vec_op_const.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/18 16:09:52 by lmarques          #+#    #+#             */
-/*   Updated: 2018/03/22 19:36:01 by lmarques         ###   ########.fr       */
+/*   Updated: 2018/03/24 00:05:13 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,20 @@
 
 t_vec3	sum3_c(t_vec3 a, float b)
 {
-	a.x += b;
-	a.y += b;
-	a.z += b;
-	return (a);
+	return ((t_vec3){a.x + b, a.y + b, a.z + b});
 }
 
 t_vec3	diff3_c(t_vec3 a, float b)
 {
-	a.x -= b;
-	a.y -= b;
-	a.z -= b;
-	return (a);
+	return ((t_vec3){a.x - b, a.y - b, a.z - b});
 }
 
 t_vec3	prod3_c(t_vec3 a, float b)
 {
-	a.x *= b;
-	a.y *= b;
-	a.z *= b;
-	return (a);
+	return ((t_vec3){a.x * b, a.y * b, a.z * b});
 }
 
 t_vec3	quo3_c(t_vec3 a, float b)
 {
-	a.x /= b;
-	a.y /= b;
-	a.z /= b;
-	return (a);
+	return ((t_vec3){a.x / b, a.y / b, a.z / b});
 }
